9deb0b7747a315598cbbbb2cdd4a11683b1c95e9
Alex Viglione <alexxe33@gmail.com>
2020-05-13 18:37:32 +0200

    Move sounds to assets

--- pages/sound_test/index.vue
+++ pages/sound_test/index.vue
@@ -2,5 +2,25 @@
 	<div>
 		<h1>Sound test</h1>
 		<nuxt-link to="/">Back to home</nuxt-link>
+		<button @click.prevent="playSound('../../assets/metronome.wav')">Play sound</button>
+		<audio
+			id="audio"
+			:src="require('@/assets/metronome.wav')"
+			controls
+			autostart="false"
+		></audio>
 	</div>
 </template>
+
+<script>
+export default {
+	methods: {
+		playSound(sound) {
+			if (sound) {
+				let audio = new Audio(sound);
+				audio.play();
+			}
+		}
+	}
+}
+</script>
