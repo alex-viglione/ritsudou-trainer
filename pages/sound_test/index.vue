fe1809a52fa06218e4375595e98873a8dd34bffd
Alex Viglione <alexxe33@gmail.com>
2020-05-14 15:46:31 +0200

    Working metronome
    Able to set bpm and bpb
    250 bpm and upwards to avoid, starts getting janky.
    Gotta set a 240 bpm limit

--- pages/sound_test/index.vue
+++ pages/sound_test/index.vue
@@ -1,12 +1,32 @@
 <template>
 	<div>
-		<h1>Sound test</h1>
-		<nuxt-link to="/">Back to home</nuxt-link>
-		<input
-			type="button"
-			value="PLAY"
-			@click="play()"
-		>
+		<div id="wrapper">
+			<h1>Sound test</h1>
+			<nuxt-link to="/">Back to home</nuxt-link>
+			<label for="bpm">BPM</label>
+			<input
+				type="text"
+				v-model="bpm"
+				name="bpm"
+			>
+			<p>Millisecond gap : {{toMs}}</p>
+			<label for="bpb">BPB</label>
+			<input
+				type="text"
+				v-model="bpb"
+				name="bpb"
+			>
+			<input
+				type="button"
+				value="PLAY"
+				@click="play"
+			>
+			<input
+				type="button"
+				value="PAUSE"
+				@click="pause"
+			>
+		</div>
 	</div>
 </template>
 
@@ -14,19 +34,59 @@
 export default {
 	data() {
 		return {
-			audio: ''
+			normalTick: '',
+			upTick: '',
+			bpm: 60,
+			bpb: 4,
+			isPlaying: false
+		}
+	},
+	computed: {
+		toMs() {
+			return 60000 / this.bpm;
 		}
 	},
 	methods: {
 		play() {
-			// this.audio.play();
-			setInterval(() => {
-				this.audio.play();
-			}, 10);
+			let millisecs = this.toMs;
+			let bpb = this.bpb;
+			let counter = 0;
+			const downTick = this.normalTick;
+			const upTick = this.upTick;
+
+			this.isPlaying = true;
+			if (this.isPlaying) {
+				upTick.play();
+				let noteInterval = setInterval(() => {
+					counter++;
+					if (counter > bpb) {
+						counter = 1;
+					}
+					counter != bpb ? downTick.play() : upTick.play();
+					if (!this.isPlaying) {
+						downTick.pause();
+						upTick.pause();
+						this.pause(noteInterval);
+					}
+				}, millisecs);
+			}
+		},
+		pause(interval) {
+			this.isPlaying = false;
+			clearInterval(interval);
 		}
 	},
 	mounted() {
-		this.audio = new Audio(require('@/assets/metronome.wav'))
+		this.normalTick = new Audio(require('@/assets/metronome.wav'));
+		this.upTick = new Audio(require('@/assets/metronomeup.wav'))
 	}
 }
 </script>
+
+<style scoped>
+#wrapper {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+</style>
